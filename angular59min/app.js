4748e7c949ced1f5a10e510ebd054c9910e96ef1
gagan0183 <g.deepsingh1@gmail.com>
2017-12-14 00:08:55 +0530

    basic app

--- angular59min/app.js
+++ angular59min/app.js
@@ -1,4 +1,25 @@
 var app =  angular.module('HangmanApp', []);
 app.controller('GameController', ['$scope', function($scope) {
-    $scope.demo = "somestring";
+    var words = ['rat', 'cat', 'bat', 'mat'];
+    $scope.incorrectLettersChosen = [];
+    $scope.correctLettersChosen = [];
+    $scope.guesses = 6;
+    $scope.displayWord = '';
+    $scope.input = {
+        letter: ''
+    };
+    var selectRandomWord = function() {
+        var index =  Math.round(Math.random() * words.length);
+        return words[index];
+    }
+    var newGame = function() {
+        $scope.incorrectLettersChosen = [];
+        $scope.correctLettersChosen = [];
+        $scope.guesses = 6;
+        $scope.displayWord = '';
+        var selectedWord = selectRandomWord();
+        console.log(selectedWord);
+    }
+
+    newGame();
 }]);
