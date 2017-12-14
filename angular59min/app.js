e380b7de2c44d01c9c308ec00dbf638e08190e75
gagan0183 <g.deepsingh1@gmail.com>
2017-12-14 18:09:26 +0530

    comp game

--- angular59min/app.js
+++ angular59min/app.js
@@ -1,5 +1,5 @@
 var app =  angular.module('HangmanApp', []);
-app.controller('GameController', ['$scope', function($scope) {
+app.controller('GameController', ['$scope', '$timeout', function($scope, $timeout) {
     var words = ['rat', 'cat', 'bat', 'mat'];
     $scope.incorrectLettersChosen = [];
     $scope.correctLettersChosen = [];
@@ -42,13 +42,28 @@ app.controller('GameController', ['$scope', function($scope) {
         if(letter != '') {
             if(selectedWord.match(new RegExp(letter, 'ig'))) {
                 $scope.correctLettersChosen.push(letter);
-                $scope.input.letter = '';
-                //$scope.selectedWord = selectedWord.replace(new RegExp(letter, 'g'), letter);
+                for(var i = 0; i < selectedWord.length; i++) {
+                    if(selectedWord[i].toLowerCase() == $scope.input.letter) {
+                        $scope.displayWord = $scope.displayWord.slice(0,i) + $scope.input.letter + $scope.displayWord.slice(i+1);
+                    }
+                }
             }
             else {
                 $scope.incorrectLettersChosen.push(letter);
-                $scope.input.letter = '';
                 $scope.guesses -= 1;
+            }
+            $scope.input.letter = '';
+            if($scope.guesses <= 0) {
+                alert('You lost the game');
+                $timeout(function() {
+                    newGame();
+                }, 500); 
+            }
+            if($scope.displayWord.indexOf('*') == -1) {
+                alert('You won the game');
+                $timeout(function() {
+                    newGame();
+                }, 500);
             }
         }
     }
